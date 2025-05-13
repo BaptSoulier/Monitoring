fa2dcb091c43a3bbaa79ccf482c995cd80b2ade4
BaptSoulier <baptiste.soulier@ynov.com>
2025-05-13 16:10:57 +0200

    maj db

--- database/db.py
+++ database/db.py
@@ -1,2 +1,11 @@
 from flask_pymongo import PyMongo
+
 mongo = PyMongo()
+
+def init_db(app):
+    """Initialize the MongoDB connection with the Flask application."""
+    mongo.init_app(app)
+
+def get_db():
+    """Return the MongoDB database object."""
+    return mongo.db
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from webapp import app
-from database.db import init_db  
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'])
